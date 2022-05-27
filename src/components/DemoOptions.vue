203c0029c2f5cad051178cceab083e5a7e82b491
Jaime <jaimeloeuf@gmail.com>
2022-05-27 20:14:54 +0800

    Add non blocking POW calculator: 1.  Add a version of the POW finder that runs on, but does not block the     main thread, allowing user to interact with the app while the POW is     calculated, by queuing computation individually in the event loop. 2.  Add switcher to call the specific POW calculation function.

--- src/components/DemoOptions.vue
+++ src/components/DemoOptions.vue
@@ -36,6 +36,19 @@
         <div class="column"><p class="title">Results</p></div>
 
         <div class="column is-narrow">
+          <div class="select is-success">
+            <select v-model="compute_type">
+              <!-- Set compute_type to empty string if using an empty default -->
+              <!-- <option disabled value="">Please select one</option> -->
+              <option value="nb-main">
+                Non Blocking (runs on Main Thread)
+              </option>
+              <option value="blocking">Blocking</option>
+            </select>
+          </div>
+        </div>
+
+        <div class="column is-narrow">
           <button class="button is-light is-success" @click="compute">
             Compute
           </button>
@@ -70,10 +83,14 @@ export default defineComponent({
       difficulty: 3,
       blockData: "",
       loading: false,
+
+      compute_type: "nb-main",
+
       pow: 0,
       results: "",
-      time: "", // Time in seconds after converting with toFixed()
 
+      // Time it took to calculate the POW number in milliseconds
+      time: 0,
       startTime: 0,
     };
   },
@@ -85,23 +102,39 @@ export default defineComponent({
       // Reset the pow and results
       this.pow = 0;
       this.results = "";
-      this.time = "";
-
-      // Set loading UI
-      this.loading = true;
+      this.time = 0;
 
       // Get start time to compute time of hashing later
       this.startTime = performance.now();
 
-      // Using setTimeout 0 to queue the blocking computeResults call, after DOM UI has been udpated.
-      // Without this, the UI wont show the loading UI before the start of the UI being blocked.
-      setTimeout(() => {
-        this.computeResults_blocking();
-        this.loading = false;
-        this.time = ((performance.now() - this.startTime) / 1000).toFixed(2);
-      }, 0);
+      switch (this.compute_type) {
+        case "nb-main":
+          return this.computeResults_nonBlocking();
+
+        case "blocking":
+          // Set loading UI first as UI will freeze
+          this.loading = true;
+
+          // Using setTimeout 0 to queue the blocking computeResults call, after DOM UI has been udpated.
+          // Without this, the UI wont show the loading UI before the start of the UI being blocked.
+          return setTimeout(() => {
+            this.computeResults_blocking();
+
+            // Calculate time taken right after computation completes before doing anything else
+            this.time = performance.now() - this.startTime;
+
+            this.loading = false;
+          }, 0);
+
+        default:
+          return alert("Internal Error: Invalid compute type selected");
+      }
     },
 
+    /**
+     * Compute results on the main UI thread directly, blocking all UI updates
+     * making it seems to the user as though the application hang / froze up.
+     */
     computeResults_blocking() {
       const leadingZeros = "0".repeat(this.difficulty);
 
@@ -111,6 +144,30 @@ export default defineComponent({
       } while (hash.substring(0, this.difficulty) !== leadingZeros);
 
       this.results = hash;
+    },
+
+    /**
+     * Compute results on the main UI thread without blocking UI updates
+     * by queuing each computation results at the end of the events loop
+     */
+    computeResults_nonBlocking() {
+      const leadingZeros = "0".repeat(this.difficulty);
+
+      // Inner function uses arrow syntax to keep the function's 'this' binding
+      const inner = () => {
+        let hash: string = sha256(this.blockData + this.pow++).toString();
+
+        if (hash.substring(0, this.difficulty) !== leadingZeros) {
+          // Queue a callback to itself once UI updates are completed
+          setTimeout(inner, 0);
+        } else {
+          // Calculate time taken right after computation completes before doing anything else
+          this.time = performance.now() - this.startTime;
+          this.results = hash;
+        }
+      };
+
+      inner();
     },
   },
 });
