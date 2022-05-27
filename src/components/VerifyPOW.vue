a5bb1ca51a3519858d546d1de4d8c829b18c27b2
Jaime <jaimeloeuf@gmail.com>
2022-05-27 20:29:32 +0800

    Create Verify POW component.

---
+++ src/components/VerifyPOW.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="section">
+    <div class="columns is-multiline">
+      <div class="column">
+        <p class="title">Verify POW</p>
+      </div>
+
+      <div class="column is-narrow">
+        <button class="button is-light is-danger is-fullwidth">Reset</button>
+      </div>
+    </div>
+
+    <div class="box">
+      <label>
+        Set a difficulty level (How many starting zeros)
+
+        <input
+          v-model="difficulty"
+          type="number"
+          class="input"
+          placeholder="Difficulty level"
+          @keypress.enter="verify"
+        />
+      </label>
+    </div>
+
+    <div class="box">
+      <label>
+        Enter your data
+
+        <input
+          v-model="blockData"
+          type="text"
+          class="input"
+          placeholder="Whatever data you want in your block"
+          @keypress.enter="verify"
+        />
+      </label>
+    </div>
+
+    <div class="box">
+      <label>
+        Enter POW number to verify
+
+        <input
+          v-model="pow"
+          type="number"
+          class="input"
+          placeholder="POW number"
+          @keypress.enter="verify"
+        />
+      </label>
+    </div>
+
+    <div class="box">
+      <div class="columns is-vcentered">
+        <div class="column"><p class="subtitle">Verify</p></div>
+
+        <div class="column is-narrow">
+          <button class="button is-light is-success" @click="verify">
+            Verify
+          </button>
+        </div>
+      </div>
+
+      <span v-if="verified !== undefined">
+        POW is <code>{{ verified ? "correct" : "wrong" }} </code>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import sha256 from "crypto-js/sha256";
+
+export default defineComponent({
+  name: "Verify",
+
+  data(): {
+    difficulty: number;
+    blockData: string;
+    pow?: number;
+    verified?: boolean;
+  } {
+    return {
+      difficulty: 3,
+      blockData: "",
+      pow: undefined,
+      verified: undefined,
+    };
+  },
+
+  methods: {
+    verify() {
+      if (!this.pow) return alert("POW number is not entered");
+
+      const leadingZeros = "0".repeat(this.difficulty);
+
+      // @todo Need to -1 because of the ++ used when calculating the POW
+      let hash = sha256(this.blockData + (this.pow - 1)).toString();
+
+      this.verified = hash.substring(0, this.difficulty) === leadingZeros;
+    },
+  },
+});
+</script>
