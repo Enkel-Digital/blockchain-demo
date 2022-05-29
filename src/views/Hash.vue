2020998fc2e29628de4e38abd8f639508b1d0a41
Jaime <jaimeloeuf@gmail.com>
2022-05-29 15:24:28 +0800

    Allow different hash functions to be used.

--- src/views/Hash.vue
+++ src/views/Hash.vue
@@ -26,20 +26,57 @@
     </div>
 
     <div class="box">
-      Hash: <code style="word-wrap: break-word">{{ hash }}</code>
+      <div class="columns is-vcentered">
+        <div class="column"><p class="subtitle is-4">Hash</p></div>
+
+        <div class="column is-narrow">
+          <div class="select is-success is-fullwidth">
+            <select v-model="hash_function">
+              <option value="SHA256">SHA256</option>
+              <option value="SHA3">SHA3</option>
+            </select>
+          </div>
+        </div>
+      </div>
+
+      <code style="word-wrap: break-word">{{ hash }}</code>
+
+      <details class="content mt-4">
+        <summary style="cursor: pointer">Types of Hashes</summary>
+        <ul>
+          <li>
+            <b>SHA256</b> is a hash function from the SHA2 hash function family
+            with a 256 bit output length.
+          </li>
+          <li>
+            <b>SHA256</b> is one of the most popular hash functions, and is used
+            by Bitcoin. This will also be the hash function used for the Proof
+            of Work demo later on.
+          </li>
+          <li>
+            <b>SHA3</b> is a hash function from the SHA3 hash function family
+            with a 256 bit output length.
+          </li>
+          <li>
+            <b>SHA3</b> is supposed to be more secure than <b>SHA256</b>, and it
+            is used by the Ethereum blockchain.
+          </li>
+        </ul>
+      </details>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import sha256 from "crypto-js/sha256";
 
 export default defineComponent({
   name: "Hash",
 
   data() {
     return {
+      hash_function: "SHA256",
+
       input: "",
       hash: "",
     };
@@ -50,6 +87,11 @@ export default defineComponent({
     input() {
       this.updateHash();
     },
+
+    // Watcher to call updateHash on change of hash function
+    hash_function() {
+      this.updateHash();
+    },
   },
 
   // Run updateHash on created so to show the hash of the empty input
@@ -58,8 +100,35 @@ export default defineComponent({
   },
 
   methods: {
-    updateHash() {
-      this.hash = sha256(this.input).toString();
+    /**
+     * Caller to await the results to get the hash function lazily
+     */
+    async getHashFunction() {
+      // These es modules have a default export which is the hash function itself,
+      // so they are parsed out before returning to the caller.
+
+      switch (this.hash_function) {
+        case "SHA256":
+          return import("crypto-js/sha256").then(({ default: fn }) => fn);
+
+        case "SHA3":
+          return import("crypto-js/sha3").then(({ default: fn }) => fn);
+
+        default: {
+          const err = new Error("Invalid hash function selected!");
+          alert(err);
+          throw err;
+        }
+      }
+    },
+
+    async updateHash() {
+      // Lazily load the hash function
+      const fn = await this.getHashFunction();
+
+      // Output length config is required because the default length for the SHA3 function is 512bits
+      // So this is needed to set to use sha3-256, and the sha256 function will simply ignore this extra config object.
+      this.hash = fn(this.input, { outputLength: 256 }).toString();
     },
 
     reset() {
