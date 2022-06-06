183d3905d77e13e8fd7d339c8244388e35972f96
Jaime <jaimeloeuf@gmail.com>
2022-06-06 21:01:16 +0800

    Update Signatures view's UI layout.

--- src/views/Signatures.vue
+++ src/views/Signatures.vue
@@ -10,9 +10,105 @@
   <div>
     <!-- Overriding section class's y axis padding to stay consistent with the different POW components -->
     <div class="section py-0">
-      <div class="columns is-multiline">
-        <div class="column is-full">
+      <div class="columns is-multiline is-vcentered">
+        <div class="column">
           <p class="title">Digital Signatures</p>
+        </div>
+
+        <div class="column is-narrow">
+          <button class="button is-light is-danger is-fullwidth" @click="reset">
+            Reset
+          </button>
+        </div>
+
+        <div class="column is-full">
+          <div class="box">
+            <p class="subtitle mb-0">Keys</p>
+
+            <div class="content">
+              <ul>
+                <li>
+                  Either click generate to create a new secret/public key pair
+                  for this demo
+                </li>
+                <li>Or, click input to enter your own key pair</li>
+              </ul>
+            </div>
+
+            <div class="columns">
+              <div class="column is-half">
+                <button
+                  class="button is-warning is-light is-fullwidth"
+                  @click="useInput"
+                >
+                  Input
+                </button>
+              </div>
+
+              <div class="column is-half">
+                <button
+                  class="button is-success is-light is-fullwidth"
+                  @click="generateKeys"
+                >
+                  Generate
+                </button>
+              </div>
+            </div>
+
+            <!-- @todo Change color just like create/verify sig for the generate and input buttons -->
+
+            <!-- If any of the options are selected, show a divisor -->
+            <div v-if="inputKeys !== undefined">
+              <hr />
+            </div>
+
+            <div v-if="inputKeys === true">
+              <div class="field has-addons">
+                <div class="control is-expanded">
+                  <input
+                    v-model="secretKey"
+                    type="text"
+                    class="input"
+                    placeholder="Secret Key"
+                  />
+                </div>
+                <div class="control">
+                  <button class="button is-info">Use</button>
+                </div>
+              </div>
+            </div>
+
+            <div v-else-if="inputKeys === false">
+              <div class="mb-2">
+                Private Key: <code class="word-wrap">{{ secretKey }}</code>
+              </div>
+
+              Public Key:
+              <code class="word-wrap">{{ publicKey }}</code>
+            </div>
+          </div>
+        </div>
+
+        <div class="column is-full">
+          <hr />
+        </div>
+
+        <div class="column is-full">
+          <div class="box">
+            <label>
+              <p class="subtitle mb-2">Enter your data</p>
+
+              <textarea
+                v-model="input"
+                class="textarea"
+                placeholder="Whatever data you want to sign or verify"
+              ></textarea>
+            </label>
+          </div>
+        </div>
+
+        <div class="column is-full">
+          <hr />
         </div>
 
         <div class="column is-half">
@@ -35,47 +131,20 @@
           </button>
         </div>
 
-        <div class="column is-full">
-          <hr />
-        </div>
-
-        <div class="column">
-          <p class="title">Generate Keys</p>
-        </div>
-
-        <div class="column is-narrow">
-          <button
-            class="button is-success is-light is-fullwidth"
-            @click="generateKeys"
-          >
-            Generate
-          </button>
-        </div>
-
-        <div v-if="secretKey && publicKey" class="column is-full">
-          <div class="box">
-            Private Key:
-            <code class="word-wrap">{{ secretKey }}</code>
-            <br />
-
-            Public Key:
-            <code class="word-wrap">{{ publicKey }}</code>
-          </div>
-        </div>
-
-        <div v-else class="column is-full">
-          Click generate to create a new secret/public key pair.
+        <!-- Only show this section once user clicks create or verify signature -->
+        <div
+          v-if="tab === 'SignatureCreate' || tab === 'SignatureVerify'"
+          class="column is-full"
+        >
+          <!--
+            Dynamic component used based on the tab selected
+            Reference: https://vuejs.org/guide/essentials/component-basics.html#dynamic-components
+          -->
+          <!-- @todo Pass in the values as props -->
+          <component :is="tab" />
         </div>
       </div>
     </div>
-
-    <hr />
-
-    <!--
-      Dynamic component used based on the tab selected
-      Reference: https://vuejs.org/guide/essentials/component-basics.html#dynamic-components
-    -->
-    <component :is="tab" />
   </div>
 </template>
 
@@ -96,22 +165,47 @@ export default defineComponent({
   components: { SignatureCreate, SignatureVerify },
 
   data(): {
-    tab: "SignatureCreate" | "SignatureVerify";
+    tab?: "SignatureCreate" | "SignatureVerify";
 
+    inputKeys?: boolean;
     publicKey?: string;
     secretKey?: string;
+
+    input: string;
   } {
     // Default to show create Signature tab
     return {
-      tab: "SignatureCreate",
+      // Defaults to undefined so that the UI shows neither
+      tab: undefined,
+
+      // Defaults to undefined so that the UI shows neither
+      inputKeys: undefined,
 
       publicKey: undefined,
       secretKey: undefined,
+
+      input: "",
     };
   },
 
   methods: {
+    reset() {
+      // Reset all data variables
+    },
+
+    useInput() {
+      // Set this variable to update the UI to show the input keys UI
+      this.inputKeys = true;
+
+      // Reset the key pair in case user generated it before hand
+      this.publicKey = "";
+      this.secretKey = "";
+    },
+
     generateKeys() {
+      // Set this variable to update the UI to stop showing the input keys UI
+      this.inputKeys = false;
+
       // Generate keys
       const key = EC.genKeyPair();
       this.publicKey = key.getPublic().encode("hex", true); // Regardless if compact or not same results
